505ac70569e75d11e1968ef8e88a3f76f5bbc8bd
boshka0 <nastyaboksha@gmail.com>
2018-09-13 23:35:47 +0300

    Implement class Sorter

--- src/index.js
+++ src/index.js
@@ -1,30 +1,48 @@
 class Sorter {
   constructor() {
     // your implementation
+    this.sorter = [];
   }
 
   add(element) {
     // your implementation
+    this.sorter.push(element);
   }
 
   at(index) {
     // your implementation
+    return this.sorter[index];
   }
 
   get length() {
     // your implementation
+    return this.sorter.length;
   }
 
   toArray() {
     // your implementation
+    return this.sorter;
   }
 
   sort(indices) {
     // your implementation
+    var array = [];
+    indices.sort((a,b)=>a-b);
+    for(var n = 0; n < indices.length; n++){
+      array.push(this.sorter[indices[n]]);
+    } 
+    if(this.compareFunction) 
+      array.sort(this.compareFunction);
+    else array.sort((a,b)=>a-b);
+    for(var n = 0; n < indices.length; n++){
+      this.sorter[indices[n]] = array[n];
+    }
+    return this.sorter;
   }
 
   setComparator(compareFunction) {
     // your implementation
+    this.compareFunction = compareFunction;
   }
 }
 
